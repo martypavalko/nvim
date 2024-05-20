6458d0e49b869694873f9fb5c0ead8ee9d5c3980
Marty Pavalko <martypavalko@gmail.com>
2024-05-20 11:30:51 -0400

    initial setup for lsp

--- lua/marty/lazy.lua
+++ lua/marty/lazy.lua
@@ -15,17 +15,16 @@ require("lazy").setup({
     {
         "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate",
-        config = function () 
-            local configs = require("nvim-treesitter.configs")
-
-            configs.setup({
-                ensure_installed = { "python", "go", "lua", "vim", "vimdoc" },
-                sync_install = false,
-                highlight = { enable = true },
-                indent = { enable = true },  
-            })
-        end
     },
+    {'williamboman/mason.nvim'},
+    {'williamboman/mason-lspconfig.nvim'},
+    {'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
+    {'neovim/nvim-lspconfig'},
+    {'hrsh7th/cmp-nvim-lsp'},
+    {'hrsh7th/nvim-cmp'},
+    {'hrsh7th/cmp-buffer'},
+    {'hrsh7th/cmp-path'},
+    {'L3MON4D3/LuaSnip'},
     {
         "eldritch-theme/eldritch.nvim",
         lazy = false,
