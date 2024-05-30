6d9cee1a91865209f698eca30ee2d0e4ebf9b7b5
Marty Pavalko <martypavalko@gmail.com>
2024-05-30 12:56:58 -0400

    added indent lines

--- lua/marty/lazy.lua
+++ lua/marty/lazy.lua
@@ -56,5 +56,10 @@ require("lazy").setup({
     {
         'nvim-telescope/telescope.nvim', tag = '0.1.6',
         dependencies = { 'nvim-lua/plenary.nvim' }
-    }
+    },
+    {
+        'lukas-reineke/indent-blankline.nvim',
+        main = 'ibl',
+        opts = {}
+    },
 })
