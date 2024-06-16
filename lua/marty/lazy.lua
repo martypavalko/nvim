011ffa5461a30dff70c42658a14ef9dc16faab9a
Marty Pavalko <marty@martypavalko.com>
2024-06-16 12:57:14 -0400

    added todo highlighting

--- lua/marty/lazy.lua
+++ lua/marty/lazy.lua
@@ -95,6 +95,15 @@ require("lazy").setup({
         }
     },
     {
+        "folke/todo-comments.nvim",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        opts = {
+          -- your configuration comes here
+          -- or leave it empty to use the default settings
+          -- refer to the configuration section below
+        }
+    },
+    {
         'akinsho/bufferline.nvim', version = "*", dependencies = 'nvim-tree/nvim-web-devicons'
     },
 })
