3c531824963cdbd67d8dafb01ef9a1c1223d1853
Marty Pavalko <marty@martypavalko.com>
2024-06-16 23:56:52 -0400

    added uniformity to telescope keymaps

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -9,6 +9,6 @@ require("telescope").setup({
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
-vim.keymap.set('n', '<leader>/', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>f/', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
