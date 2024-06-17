f0415dd10206b23ef16b4fa2db54135ff4a7f1d7
Marty Pavalko <marty@martypavalko.com>
2024-06-17 00:45:47 -0400

    catppuccin integrations

--- after/plugin/color.lua
+++ after/plugin/color.lua
@@ -3,6 +3,42 @@ require("catppuccin").setup({
     background = {
         light = "latte",
         dark = "mocha",
+    },
+    integrations = {
+        require("lualine").setup {
+            options = {
+                theme = "catppuccin"
+            }
+        },
+        mini = {
+            enabled = true,
+            independent_color = "",
+        },
+        cmp = true,
+        treesitter = true,
+        telescope = {
+            enabled = true,
+        },
+        native_lsp = {
+            enabled = true,
+            virtual_text = {
+                errors = { "italic" },
+                hints = { "italic" },
+                warnings = { "italic" },
+                information = { "italic" },
+                ok = { "italic" },
+            },
+            underlines = {
+                errors = { "underline" },
+                hints = { "underline" },
+                warnings = { "underline" },
+                information = { "underline" },
+                ok = { "underline" },
+            },
+            inlay_hints = {
+                background = true,
+            },
+        },
     }
 })
 
