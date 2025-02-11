3677d5e8aeb455eaa14c7202dbe1f477341b2f13
martypavalko <mpavalko@avifoodsystems.com>
2025-02-10 19:06:21 -0500

    Lualine testing

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -7,7 +7,7 @@ require('lualine').setup {
         lualine_a = {'mode'},
         lualine_b = {'branch', 'diff', 'diagnostics'},
         lualine_c = {'buffers'},
-        lualine_x = {'filesize', 'filetype'},
+        -- lualine_x = {'filesize', 'filetype'},
         -- lualine_y = {'progress'},
         lualine_y = {'selectioncount'},
         lualine_z = {'location'}
