6f757de375a7fb2247f83d156b4e2de7ef7fae82
martypavalko <mpavalko@avifoodsystems.com>
2025-03-11 23:39:31 -0400

    feat: added qol for golang

--- lua/marty/lazy.lua
+++ lua/marty/lazy.lua
@@ -163,4 +163,18 @@ require("lazy").setup({
         { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
         }
     },
+    {
+      "ray-x/go.nvim",
+      dependencies = {  -- optional packages
+        "ray-x/guihua.lua",
+        "neovim/nvim-lspconfig",
+        "nvim-treesitter/nvim-treesitter",
+      },
+      config = function()
+        require("go").setup()
+      end,
+      event = {"CmdlineEnter"},
+      ft = {"go", 'gomod'},
+      build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
+    }
 })
