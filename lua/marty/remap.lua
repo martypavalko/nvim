96e22f5262761ba8fb45c739e059a2997f033527
martypavalko <mpavalko@avifoodsystems.com>
2025-08-26 16:19:51 -0400

    insane qol with visual mode indenting

--- lua/marty/remap.lua
+++ lua/marty/remap.lua
@@ -10,6 +10,9 @@ keymap.set("n", "<leader>e", vim.cmd.NvimTreeToggle)
 keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+keymap.set("v", "<", "<gv")
+keymap.set("v", ">", ">gv")
+
 keymap.set({"n", "v"}, "<leader>y", [["+y]])
 keymap.set("n", "<leader>Y", [["+Y]])
 keymap.set("x", "<leader>p", [["_dP]])
