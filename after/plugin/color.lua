4ef1e323a8cd6323b486044aa4221877f3587d8d
martypavalko <mpavalko@avifoodsystems.com>
2025-08-28 10:46:28 -0400

    color settings

--- after/plugin/color.lua
+++ after/plugin/color.lua
@@ -1,53 +1,56 @@
 require("catppuccin").setup({
-    flavour = "mocha",
-    background = {
-        light = "latte",
-        dark = "mocha",
-    },
-    integrations = {
-        require("lualine").setup {
-            options = {
-                theme = "catppuccin"
-            }
-        },
-        mini = {
-            enabled = true,
-            independent_color = "",
-        },
-        cmp = true,
-        treesitter = true,
-        gitsigns = true,
-        telescope = {
-            enabled = true,
-        },
-        native_lsp = {
-            enabled = true,
-            virtual_text = {
-                errors = { "italic" },
-                hints = { "italic" },
-                warnings = { "italic" },
-                information = { "italic" },
-                ok = { "italic" },
-            },
-            underlines = {
-                errors = { "underline" },
-                hints = { "underline" },
-                warnings = { "underline" },
-                information = { "underline" },
-                ok = { "underline" },
-            },
-            inlay_hints = {
-                background = true,
-            },
-        },
-    }
+	flavour = "macchiato",
+	default_integrations = false,
+	integrations = {
+		cmp = true,
+		flash = true,
+		-- gitsigns = true,
+		gitsigns = {
+			enabled = true,
+			transparent = false,
+		},
+		harpoon = true,
+		require("lualine").setup({
+			options = {
+				theme = "catppuccin",
+			},
+		}),
+		mini = {
+			enabled = true,
+			independent_color = "",
+		},
+		nvimtree = true,
+		telescope = {
+			enabled = true,
+		},
+		treesitter = true,
+		native_lsp = {
+			enabled = true,
+			virtual_text = {
+				errors = { "italic" },
+				hints = { "italic" },
+				warnings = { "italic" },
+				information = { "italic" },
+				ok = { "italic" },
+			},
+			underlines = {
+				errors = { "underline" },
+				hints = { "underline" },
+				warnings = { "underline" },
+				information = { "underline" },
+				ok = { "underline" },
+			},
+			inlay_hints = {
+				background = true,
+			},
+		},
+	},
 })
 
 function ColorMyPencils(color)
-    color = color or "catppuccin"
-    vim.cmd.colorscheme(color)
-    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+	vim.cmd.colorscheme(color)
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
-ColorMyPencils()
+ColorMyPencils("catppuccin")
