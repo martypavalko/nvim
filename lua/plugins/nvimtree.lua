f9f1f54907483b8b659a023f28e47443d98070a0
martypavalko <marty@martypavalko.com>
2026-03-01 14:11:32 -0500

    add easy-dotnet support to nvim-tree

--- lua/plugins/nvimtree.lua
+++ lua/plugins/nvimtree.lua
@@ -38,6 +38,23 @@ return {
           enable = true,
           update_root = false,
         },
+        on_attach = function(bufnr)
+          local api = require("nvim-tree.api")
+
+          local function opts(desc)
+            return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+          end
+
+          -- Default mappings
+          api.config.mappings.default_on_attach(bufnr)
+
+          -- Custom mapping for easy-dotnet integration
+          vim.keymap.set("n", "A", function()
+            local node = api.tree.get_node_under_cursor()
+            local path = node.type == "directory" and node.absolute_path or vim.fs.dirname(node.absolute_path)
+            require("easy-dotnet").create_new_item(path)
+          end, opts("Create file from dotnet template"))
+        end,
       })
     end,
   },
